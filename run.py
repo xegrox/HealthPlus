d61dd31b040d0ce72e0f01780db56be207528807
XeGrox <xegrox@protonmail.com>
2022-01-11 21:25:35 +0800

    fix(run): fix crash when not debugging

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from livereload import Server
 from app import app
 
 if __name__ == '__main__':
-    if os.environ['DEBUG'] == "1":
+    if os.environ.get('DEBUG') == "1":
         app.debug = True
         server = Server(app.wsgi_app)
         server.serve(host='0.0.0.0', liveport=35729)
     else:
-        app.run()
+        app.run(host='0.0.0.0')
